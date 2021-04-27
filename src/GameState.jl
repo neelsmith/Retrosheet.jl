c29faf9e982bae461be1d6a91803d424015fb54c
Neel Smith <dnsmith.neel@gmail.com>
2021-04-27 13:29:12 -0400

    Add GameState struct

---
+++ src/GameState.jl
@@ -0,0 +1,15 @@
+
+"""Variables maintaing state of a game in play.
+
+$(SIGNATURES)
+"""
+struct GameState
+    scorehome::Int64
+    scorevisitor::Int64
+    inning::Int64
+    outs::Int64
+    runner1
+    runner2
+    runner3
+    lastplay::AbstractString
+end
