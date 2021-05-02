e6f6be06ef29194b409a788425d03fa6d2c678cc
Neel Smith <nsmith@Technites.local>
2021-05-02 06:38:42 -0400

    Add lineup functions

--- test/test_gamerecord.jl
+++ test/test_gamerecord.jl
@@ -5,3 +5,21 @@
     expected = "19790801BOS0"
     @test  Retrosheet.gameid(gameday, hometeam) == expected
 end
+
+@testset "Test extracting basic game info from record" begin
+    sched60sox = season(1960, "BOS")
+    game1record = sched60sox[1] |> gameid |> gamerecord
+    basicinfo = gameinfo(game1record)
+    @test basicinfo.hometeam == "WS1"
+    @test basicinfo.visitor == "BOS"
+    @test basicinfo.stadium == "WAS09"
+end
+
+
+@testset "Test extracting lineups from record" begin
+    sched60sox = season(1960, "BOS")
+    game1record = sched60sox[1] |> gameid |> gamerecord
+    players = lineup(game1record)
+    @test length(players) == 18
+    @test players[1].player == "Pumpsie Green"
+end
